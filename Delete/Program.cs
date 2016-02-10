1fb58f3f48b17adac8e8d06f3756237e33160584
Emma121 <emma_movsesyan#edu.aua.am>
2016-02-11 01:11:37 +0400

    I added Hello World.

--- Delete/Program.cs
+++ Delete/Program.cs
@@ -17,6 +17,7 @@ namespace Delete
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            System.Console.Write("Hello World");
         }
     }
 }
